7f67c2e1bbe3a239c1df3ed399646967dcc119db
oneinchlunch <amir1mrkonjic@gmail.com>
2022-01-24 03:01:10 +0100

    added post

--- src/components/homepage/views/AdminView.tsx
+++ src/components/homepage/views/AdminView.tsx
@@ -1,17 +1,45 @@
 import { Button } from 'react-bootstrap';
 import { useGlobalState } from "../../../GlobalStateProvider";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const AdminView = () => {
     const { state } = useGlobalState();
+    const [ users, setUsers ] = useState<any>([]);
+
+    const getData = async() => {
+        await axios.get("http://localhost:3001/users").then((res) => {
+        setUsers((res.data));
+      })
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     const logit = () => {
-        console.log(state);
-    }
+        console.log(users);
+    };
+
+    const handleAddUser = () => {
+        axios.post("http://localhost:3001/insert", {
+            id: null,
+            active: 1,
+            admin: 0,
+            username: "test2",
+            password: "test2"
+        }).then(() => { 
+            getData();
+        });
+    };
 
     return (
     <>
         <Button onClick={logit}>
             logit
+        </Button>
+        <Button onClick={handleAddUser}>
+            add user
         </Button>
     </>
     );
